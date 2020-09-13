72a4596bdc75f097dc3588405b02fc0154d1bd2b
jandiy <vargas11197@gmail.com>
2020-09-13 16:26:05 -0400

    actualizando datos jandira 1

--- config/database.php
+++ config/database.php
@@ -62,8 +62,8 @@ return [
             'port' => $DATABASE_URL['port'],
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL['user'],
-            'password' => $DATABASE_URL['pass'],
-            /*'host' => env('DB_HOST', '127.0.0.1'),
+            'password' => $DATABASE_URL['pass'],/*
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
