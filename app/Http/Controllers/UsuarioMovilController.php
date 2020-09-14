a143600f89c13a82d27e78ebbe685513723774fa
jandiy <vargas11197@gmail.com>
2020-09-14 17:17:50 -0400

    loguear api 1 14092020

--- app/Http/Controllers/UsuarioMovilController.php
+++ app/Http/Controllers/UsuarioMovilController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioMovilController extends Controller
 {
-    public function api (){
-        $user = DB::table('users')->get();
+    public function loguear (Request $request){
+        $usuario = $request->usuario;
+        $password = $request->password;
+        $user = DB::table('users')
+                ->where( 'usuario', '=', $usuario )
+                ->get();
+        if(count($user) == 0){
+            $user = 'error';   
+        }else{
+            if (!(Hash::check($password, $user[0]->password))){
+                $user = 'error';
+            }
+        }
+               
         return response()->json([
             'users'   => $user
         ]);
