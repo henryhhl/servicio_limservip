eb638bfb30ca15c2542ad5ddc0a33e3a236c221d
jandiy <vargas11197@gmail.com>
2020-09-23 18:09:54 -0400

    UsuarioMovilController 23/09/2020 jandira

--- app/Http/Controllers/UsuarioMovilController.php
+++ app/Http/Controllers/UsuarioMovilController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Cliente;
 
 class UsuarioMovilController extends Controller
 {
@@ -25,5 +27,60 @@ class UsuarioMovilController extends Controller
         return response()->json([
             'data'   => $user
         ]);
+    }
+    
+    public function registrar(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $nombre = $request->input('nombre');
+            $apellido = $request->input('apellido');
+            $nit = $request->input('nit');
+            $email = $request->input('email');
+            $contacto = $request->input('contacto');
+            $foto = $request->input('foto');
+            $usuario = $request->input('usuario');
+            $password = $request->input('password');
+
+            $value = User::where('usuario', '=', $usuario)->get();
+
+            if (sizeof($value) > 0) {
+                DB::rollBack();
+                return response()->json([
+                    'data' => [],
+                ]);
+            }
+
+
+            $user = new User();
+            $user->nombre = $nombre;
+            $user->apellido = $apellido;
+            $user->email = $email;
+            $user->usuario = $usuario;
+            $user->imagen = $foto;
+            $user->password = bcrypt($password);
+            $user->save();
+
+            $cliente = new Cliente();
+            $cliente->idusuario = $user->id;
+            $cliente->nit = $nit;
+            $cliente->contacto = $contacto;
+            $cliente->save();
+
+            DB::commit();
+            
+            $ultimoU=DB::table('users')->orderBy('id','desc')->first();
+            //dd($ultimoU);
+            return response()->json([
+                'data'      => [$ultimoU],
+            ]);
+
+        }catch(\Exception $th) {
+            DB::rollBack();
+            return response()->json([
+                'data' => [],
+            ]);
+        }
     }
 }
