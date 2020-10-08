744a392f0ee3e9ab75653a5850567a4a9d107663
jandiy <vargas11197@gmail.com>
2020-10-08 19:52:03 -0400

    subir jandira 10/08/2020

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::get('solicitud/personal', 'SolicitudMovilController@personalAsignado');
 Route::post('solicitud/registrar', 'SolicitudMovilController@store');
 
 Route::get('solicitud/notificacion', 'SolicitudMovilController@get_notificacionMovil');
+
 Route::get('solicitud/versolicitud', 'SolicitudMovilController@verSolicitud');
